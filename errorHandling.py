c0b2d67897471f62ae0079d7b57b52bde55d4609
Alexei Barulin <74567272+lolchicer@users.noreply.github.com>
2022-10-06 13:38:59 +0300

    -checkName(update), checkCredentials(update)

--- errorHandling.py
+++ errorHandling.py
@@ -17,40 +17,6 @@ def checkPermission(update: Update) -> None:
         raise NoPermission()
 
 
-def checkName(update: Update) -> bool:
-    RESHETNIKOVA_ALIAS = ['reshetnikova', 'resh', 'r']
-    LITEYNII_ALIAS = ['liteynii', 'liteinii', 'litei', 'l', 'litey', 'lit']
-    HOME_ALIAS = ['home', 'h']
-
-    msgWords = update.message.text.split()
-    
-    for item in [
-        tuple('Reshetnikova', RESHETNIKOVA_ALIAS),
-        tuple('Liteynii', LITEYNII_ALIAS),
-        tuple('Home', HOME_ALIAS)
-    ]:
-        if msgWords[1].lower() in item[2]:
-            mikrotikName = item[1]
-    
-    if mikrotikName == None:
-        update.message.reply_markdown_v2('Server doesn\'t recognize this name of Mikrotik\. Try another one')
-    
-    return mikrotikName
-
-
-def checkCredentials(update: Update) -> bool:
-    mikrotikName = checkName(update)
-
-    if mikrotikName == None:
-        return
-    
-    mikrotikCredentials = mikrotikFunctions.TryGetMikrotikCredentials(mikrotikName)
-    if mikrotikCredentials == False:
-        update.message.reply_markdown_v2('Some problem with getting mikrotik credentials\.\r\nMaybe server doesn\'t have file with this credentials\.')
-    
-    return mikrotikCredentials
-
-
 def errorHandle(update: Update, context: CallbackContext):
     error = context.error
     
