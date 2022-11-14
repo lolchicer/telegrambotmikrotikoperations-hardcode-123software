fce8a35d29aa7e10ee7246982a72ec8c45a702c1
lolchicer <lolchicer@live.ru>
2022-11-14 17:55:14 +0300

    commandsstates.py

---
+++ commandsStates.py
@@ -0,0 +1,25 @@
+CREATE = 0
+
+CREATE_CREATE = "созадние учётной записи"
+CREATE_CHECK = "проверка учётной записи"
+
+DISABLE = 1
+
+DISABLE_DISABLE = "приостановка работы учётной записи"
+DISABLE_COMMENT = "комментирование приостановки"
+
+ENABLE = 2
+
+ENABLE_ENABLE = "восстановление работы учётной записи"
+ENABLE_COMMENT = "комментирование восстановления"
+
+CHANGE_PASSWORD = 3
+
+CHANGE_PASSWORD_CHANGE_PASSWORD = "смена пароля"
+
+CHANGE_PRESHARED_KEY = 4
+
+CHANGE_PRESHARED_KEY_CHANGE_PRESHARED_KEY = "смена предварительного ключа"
+
+COMMON_VALIDATION = "проверка сообщения"
+COMMON_SENDEMAIL = "отправка уведомления на электронную почту"
